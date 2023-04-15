9a1349d53d32d7884b33045af44777ab2e763a0f
Azenga Kevin <azenga.kevin7@gmail.com>
2023-04-15 10:10:50 +0300

    WIP: Selecting date range

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,26 @@ import { ref } from "vue";
 const date = ref(new Date());
 const dateTime = ref(new Date());
 const time = ref(new Date());
+
+const range = ref({
+  start: new Date(2020, 0, 6),
+  end: new Date(2020, 0, 10)
+});
+const mode = ref('date');
+const rules = ref([
+  {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0,
+  },
+  {
+    hours: 23,
+    minutes: 59,
+    seconds: 59,
+    milliseconds: 999,
+  },
+]);
 </script>
 
 <template>
@@ -10,7 +30,9 @@ const time = ref(new Date());
     <div class="grid gap-4 grid-cols-12">
       <div class="col-span-4">
         <h2 class="text-2xl font-bold text-gray-900">Selecting The Date Only</h2>
-        <p class="mt-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae assumenda consequatur animi neque, sunt facilis illo atque rerum quae sint odit exercitationem. Eius illum sint dolorum enim nam rerum iure, commodi labore?</p>
+        <p class="mt-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae assumenda
+          consequatur animi neque, sunt facilis illo atque rerum quae sint odit exercitationem. Eius illum sint dolorum
+          enim nam rerum iure, commodi labore?</p>
       </div>
       <div class="col-span-8">
         <VDatePicker v-model="date" mode="date" is24hr hide-time-header />
@@ -19,7 +41,9 @@ const time = ref(new Date());
     <div class="grid gap-4 grid-cols-12">
       <div class="col-span-4">
         <h2 class="text-2xl font-bold text-gray-900">Selecting The Time Only</h2>
-        <p class="mt-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae assumenda consequatur animi neque, sunt facilis illo atque rerum quae sint odit exercitationem. Eius illum sint dolorum enim nam rerum iure, commodi labore?</p>
+        <p class="mt-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae assumenda
+          consequatur animi neque, sunt facilis illo atque rerum quae sint odit exercitationem. Eius illum sint dolorum
+          enim nam rerum iure, commodi labore?</p>
       </div>
       <div class="col-span-8">
         <VDatePicker v-model="time" mode="time" is24hr hide-time-header />
@@ -28,10 +52,23 @@ const time = ref(new Date());
     <div class="grid gap-4 grid-cols-12">
       <div class="col-span-4">
         <h2 class="text-2xl font-bold text-gray-900">Selecting The Date and Time</h2>
-        <p class="mt-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae assumenda consequatur animi neque, sunt facilis illo atque rerum quae sint odit exercitationem. Eius illum sint dolorum enim nam rerum iure, commodi labore?</p>
+        <p class="mt-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae assumenda
+          consequatur animi neque, sunt facilis illo atque rerum quae sint odit exercitationem. Eius illum sint dolorum
+          enim nam rerum iure, commodi labore?</p>
       </div>
       <div class="col-span-8">
         <VDatePicker v-model="dateTime" mode="dateTime" is24hr hide-time-header />
+      </div>
+    </div>
+    <div class="grid gap-4 grid-cols-12">
+      <div class="col-span-4">
+        <h2 class="text-2xl font-bold text-gray-900">Selecting Date Range</h2>
+        <p class="mt-6 text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae assumenda
+          consequatur animi neque, sunt facilis illo atque rerum quae sint odit exercitationem. Eius illum sint dolorum
+          enim nam rerum iure, commodi labore?</p>
+      </div>
+      <div class="col-span-8">
+        <VDatePicker v-model.range="range" :mode="mode" :rules="rules" />
       </div>
     </div>
   </div>
